841c32fef44723a4130bf278704824f402629162
Isaac <caasilemroh@gmail.com>
2022-12-07 15:50:53 -0600

    Fixed index file testing with Jest

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import { Button } from '.'
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(Button).toBeTruthy()
   })
-})
+});
