f6c082f91bdafba9e61bf54f7c19a33efe528f15
Isaac <caasilemroh@gmail.com>
2022-12-08 10:16:42 -0600

    Added repo info to package.json. Added build command

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,16 @@
-export * from './components';
+import React from 'react';
+
+declare type VariantType = 'primary' | 'secondary' | 'accent' | 'danger' | 'hazard' | 'warning' | 'success' | 'info' | 'disabled';
+
+declare type ShadowType = 1 | 2 | 3 | 4 | 5 | 6 | 7;
+
+declare type SizeType = 'sm' | 'md' | 'lg';
+
+interface ButtonPropsInterface extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: VariantType;
+    shadow?: ShadowType;
+    size?: SizeType;
+}
+declare const Button: (props: ButtonPropsInterface) => JSX.Element;
+
+export { Button };
